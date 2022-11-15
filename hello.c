1c404339633336868a792ef5d3faab13f12032c8
Çağdaş <cahoglc@gmail.com>
2022-11-15 12:04:46 +0300

    summation of numbers printed

--- hello.c
+++ hello.c
@@ -10,7 +10,13 @@ int main()
     printf("Enter the numbers: ");
     scanf("%d %d", &a, &b);
 
+    int result;
+
+    result = a+b;
+
+    printf("Summation of the numbers is %d", result);
     
+
 
     return 0;
 
